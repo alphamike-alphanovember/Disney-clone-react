e4ff0dfa33d6c5da0fafe73bd9f71a0688c9b5ca
alphamike-alphanovember <amansinghgautam16@gmail.com>
2021-06-20 17:42:07 +0530

    Add Viewer

--- disneyplus/src/components/Imgslider.js
+++ disneyplus/src/components/Imgslider.js
@@ -71,6 +71,7 @@ const Wrap = styled.div`
     box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
       rgb(0 0 0/ 73%) 0px 16px 10px -10px;
     transition-duration: 300ms;
+    cursor: pointer;
 
     &:hover {
       border: 4px solid rgba(249, 249, 249, 0.8);
